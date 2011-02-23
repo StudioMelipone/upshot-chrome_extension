6a6e51f1fd699c3c84c4e3822b1e288572d64816
Gregory Durelle <g.durelle@studiomelipone.eu>
2011-02-23 22:19:30 +0100

    start refont

--- upshot.js
+++ upshot.js
@@ -1,10 +1,8 @@
 // Restrieve user's id in localStorage["upshot_id"].
-function connexion(email, token){
+function connexion(){
   var xhr = new XMLHttpRequest();
-
-	var params = "email="+email+"&token="+token;
 	
-	xhr.open("GET", "http://eupchote.com/users/get_id.json?"+params, true);
+	xhr.open("GET", "http://eupchote.com/users/get_id.json", true);
   
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
@@ -13,31 +11,32 @@ function connexion(email, token){
 
       if(resp.status==200 && resp.return==true){
 				
-        document.getElementById('connection_status').textContent = "Connected";
+        // document.getElementById('connection_status').textContent = "Connected";
         localStorage["upshot_id"] = resp.id;
-  			set_user_accounts(resp.user_accounts);
+				localStorage["upshot_user_accounts"] = JSON.stringify(resp.user_accounts);
+  			// set_user_accounts(resp.user_accounts);
       } else {
         localStorage["upshot_id"] = null;
-  				localStorage["upshot_user_accounts"] = null;
+  			localStorage["upshot_user_accounts"] = null;
   				
-  				if(document.getElementById('connection_status')){
-  					document.getElementById('connection_status').textContent = "";
-  				}
-					
-					if(resp.status == 401){
-						document.getElementById('connection_status').textContent = resp.error;
-					}
-  				
-					// Clean accounts
-  				if(document.getElementById("accounts")){
-  					var accounts = document.getElementById("accounts");
-  
-	      		// remove tbody if already exists
-	      		if(accounts.getElementsByTagName('tbody')[0]!=null){
-	      			accounts.removeChild(accounts.getElementsByTagName('tbody')[0]);
-	  						accounts.style.display = "none";
-	      		}
-  				}
+  				// if(document.getElementById('connection_status')){
+  				//   					document.getElementById('connection_status').textContent = "";
+  				//   				}
+  				// 				
+  				// 				if(resp.status == 401){
+  				// 					document.getElementById('connection_status').textContent = resp.error;
+  				// 				}
+  				//   				
+  				// 				// Clean accounts
+  				//   				if(document.getElementById("accounts")){
+  				//   					var accounts = document.getElementById("accounts");
+  				//   
+  				//       		// remove tbody if already exists
+  				//       		if(accounts.getElementsByTagName('tbody')[0]!=null){
+  				//       			accounts.removeChild(accounts.getElementsByTagName('tbody')[0]);
+  				//   						accounts.style.display = "none";
+  				//       		}
+  				//   				}
       }
     }
   }
